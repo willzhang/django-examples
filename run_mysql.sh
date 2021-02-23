bdddc6454002758e2200037bc3b01536f60a7072
willzhang <willzhmic@outlook.com>
2021-02-23 17:27:26 +0800

    update

--- run_mysql.sh
+++ run_mysql.sh
@@ -3,8 +3,8 @@ docker run -d --name mysql \
   --restart always \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD=123456 \
-  -e MYSQL_DATABASE=bookstack \
-  -e MYSQL_USER=bookstack \
-  -e MYSQL_PASSWORD=bookstack123 \
+  -e MYSQL_DATABASE=library \
+  -e MYSQL_USER=library \
+  -e MYSQL_PASSWORD=library123 \
   -v mysql:/var/lib/mysql \
   mysql:8.0.23 --default-authentication-plugin=mysql_native_password
